785aecdaf90218ff27cfae6290b5995d8dc748c0
drewtato <15526875+drewtato@users.noreply.github.com>
2021-12-05 22:32:47 -0800

    Some small stuff

--- days/day06/src/main.rs
+++ days/day06/src/main.rs
@@ -18,23 +18,23 @@ fn parser() -> Input {
 fn main() {
 	let fishes = parser();
 
-	let fishes = fishes.into_iter().fold([0; 9], |mut acc, fish| {
+	let mut fishes = fishes.into_iter().fold([0; 9], |mut acc, fish| {
 		acc[fish as usize] += 1;
 		acc
 	});
 
-	count_fishes(fishes, 80);
+	count_fishes(&mut fishes, 80);
 
-	count_fishes(fishes, 256);
+	count_fishes(&mut fishes, 256 - 80);
 }
 
-fn count_fishes(mut fishes: [Number; 9], days: usize) {
+fn count_fishes(fishes: &mut [Number; 9], days: usize) {
 	for _day in 0..days {
 		let new_fish = fishes[0];
 		fishes.copy_within(1..9, 0);
 		fishes[6] += new_fish;
 		fishes[8] = new_fish;
-		display(fishes.into_iter().sum::<Number>());
+		// display(fishes.iter().copied().sum::<Number>());
 	}
-	display(fishes.into_iter().sum::<Number>());
+	display(fishes.iter().copied().sum::<Number>());
 }
