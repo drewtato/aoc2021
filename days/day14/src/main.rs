027a3173556228d7c45a6f26574a94c40b99ca4c
drewtato <15526875+drewtato@users.noreply.github.com>
2021-12-14 15:33:56 -0800

    Better day 14

--- days/day14/src/main.rs
+++ days/day14/src/main.rs
@@ -1,141 +1,107 @@
 #![feature(array_windows)]
-#![allow(unused_imports)]
+// #![allow(unused_imports)]
 use std::collections::HashMap;
-use std::mem::swap;
-use std::slice::SliceIndex;
-use std::time::Instant;
 
 use helpers::itertools::Itertools;
 use helpers::*;
-use rayon::iter::plumbing::Producer;
-use rayon::iter::{ParallelBridge, ParallelIterator};
 
-type Input = (Vec<u8>, HashMap<[u8; 2], u8>);
+type Input = (Vec<u8>, HashMap<[u8; 2], [u8; 3]>);
 
 fn parser() -> Input {
-	let re = regex::Regex::new(r"(.)(.) -> (.)").unwrap();
 	let stdin = read_stdin().unwrap();
 	let mut lines = stdin.trim().lines();
-	let start = lines.next().unwrap().as_bytes().to_vec();
+	let start = lines
+		.next()
+		.unwrap()
+		.as_bytes()
+		.iter()
+		.map(|&b| b - b'A')
+		.collect();
 	lines.next();
 	let pairs = lines
-		.map(|l| re_parse(&re, l).expect(l))
-		.map(|strs: [String; 3]| strs.map(|s| s.as_bytes()[0]))
-		.map(|[a, b, c]| ([a, b], c))
+		.map(str::as_bytes)
+		.map(|l| [l[0], l[1], l[6]])
+		.map(|arr| arr.map(|n| n - b'A'))
+		.map(|[a, b, c]| ([a, b], [a, c, b]))
 		.collect();
 	(start, pairs)
 }
 
-// These are the max values
-// const FIRST: usize = 24;
-// const TOTAL: usize = 50;
+type Count = u128;
 
-const FIRST: usize = 19;
-const TOTAL: usize = 40;
+const EXPANSION: usize = 2;
+const MAX_ITERS: usize = 40;
+
+const PART_1_ITERATIONS: usize = 10 / EXPANSION;
+const PART_2_ITERATIONS: usize = (MAX_ITERS - 10) / EXPANSION;
+
+#[allow(clippy::assertions_on_constants)]
+const _: () = assert!(EXPANSION * PART_1_ITERATIONS == 10);
+#[allow(clippy::assertions_on_constants)]
+const _: () = assert!(EXPANSION * PART_2_ITERATIONS == MAX_ITERS - 10);
 
 fn main() {
-	let (mut polymer, pair_rules) = parser();
-	let mut new_polymer = Vec::with_capacity(polymer.len() * 2);
+	let (polymer, pair_rules) = parser();
 
-	// Part 1
-	for _ in 0..10 {
-		new_polymer.clear();
-		for &[a, b] in polymer.array_windows() {
-			new_polymer.push(a);
-			new_polymer.push(pair_rules[&[a, b]]);
+	let pair_counts: HashMap<[u8; 2], HashMap<[u8; 2], Count>> = pair_rules
+		.into_iter()
+		.map(|(pair, [a, b, c])| (pair, HashMap::from([([a, b], 1), ([b, c], 1)])))
+		.collect();
+
+	let mut expanded_pair_counts = pair_counts.clone();
+	for _ in 0..(EXPANSION - 1) {
+		for counts in expanded_pair_counts.values_mut() {
+			*counts = expand_polymer(counts, &pair_counts);
 		}
-		new_polymer.push(*polymer.last().unwrap());
-		swap(&mut polymer, &mut new_polymer);
 	}
 
-	let counts = count_polymer(&polymer);
+	let mut expanded_polymer = count_elements(&polymer);
+	for _ in 0..PART_1_ITERATIONS {
+		expanded_polymer = expand_polymer(&expanded_polymer, &expanded_pair_counts);
+	}
+	display(max_minus_min(&expanded_polymer, polymer[0]));
 
-	let (min, max) = counts
+	for _ in 0..PART_2_ITERATIONS {
+		expanded_polymer = expand_polymer(&expanded_polymer, &expanded_pair_counts);
+	}
+	display(max_minus_min(&expanded_polymer, polymer[0]));
+}
+
+fn count_elements(polymer: &[u8]) -> HashMap<[u8; 2], Count> {
+	polymer
+		.array_windows()
+		.copied()
+		.counts()
+		.into_iter()
+		.map(|(k, v)| (k, v as Count))
+		.collect()
+}
+
+fn max_minus_min(expanded_polymer: &HashMap<[u8; 2], Count>, first: u8) -> Count {
+	let elements = expanded_polymer
 		.citer()
-		.map(|(_, c)| c)
-		.minmax()
-		.into_option()
-		.unwrap();
-	display(max - min);
-
-	// Part 2
-	for _ in 0..(FIRST - 10) {
-		new_polymer.clear();
-		for &[a, b] in polymer.array_windows() {
-			new_polymer.push(a);
-			new_polymer.push(pair_rules[&[a, b]]);
-		}
-		new_polymer.push(*polymer.last().unwrap());
-		swap(&mut polymer, &mut new_polymer);
-	}
-
-	let mut counts: HashMap<u8, _> = count_polymer(&polymer).into_iter().collect();
-
-	let seen_counts = pair_rules
-		.ckeys()
-		.par_bridge()
+		.map(|([_, b], n)| (b, n))
+		.chain([(first, 1)])
 		.fold(
-			|| (HashMap::new(), Vec::new(), Vec::new()),
-			|(mut seen, mut vec1, mut vec2), pair| {
-				seen.insert(
-					pair,
-					pair_insertion(pair, &mut vec1, &mut vec2, &pair_rules),
-				);
-				(seen, vec1, vec2)
+			HashMap::new(),
+			|mut counts: HashMap<u8, Count>, (elem, n)| {
+				*counts.entry(elem).or_default() += n;
+				counts
 			},
-		)
-		.map(|(seen, _, _)| seen)
-		.reduce(HashMap::new, |seen1, mut seen2| {
-			seen2.extend(seen1);
-			seen2
-		});
+		);
+	let (min, max) = elements.cvalues().minmax().into_option().unwrap();
+	max - min
+}
 
-	for &pair in polymer.array_windows() {
-		// let entry = seen_counts.entry(pair).or_insert_with(|| {
-		// 	pair_insertion(pair, &mut int_polymer, &mut new_polymer, &pair_rules)
-		// });
-		let entry = &seen_counts[&pair];
-
-		for &(p, n) in entry {
-			*counts.get_mut(&p).unwrap() += n;
+fn expand_polymer(
+	polymer: &HashMap<[u8; 2], Count>,
+	pair_counts: &HashMap<[u8; 2], HashMap<[u8; 2], Count>>,
+) -> HashMap<[u8; 2], Count> {
+	let mut new_map = HashMap::with_capacity(polymer.len());
+	for (pair, &multiplier) in polymer {
+		for (pair, count) in pair_counts[pair].citer() {
+			*new_map.entry(pair).or_default() += count * multiplier;
 		}
 	}
-
-	let (min, max) = counts
-		.citer()
-		.map(|(_, c)| c)
-		.minmax()
-		.into_option()
-		.unwrap();
-	display(max - min);
-}
-
-fn pair_insertion(
-	pair: [u8; 2],
-	int_polymer: &mut Vec<u8>,
-	new_polymer: &mut Vec<u8>,
-	pair_rules: &HashMap<[u8; 2], u8>,
-) -> Vec<(u8, u128)> {
-	int_polymer.clear();
-	int_polymer.extend(pair);
-	for _ in 0..(TOTAL - FIRST) {
-		new_polymer.clear();
-		for &[a, b] in int_polymer.array_windows() {
-			new_polymer.push(a);
-			new_polymer.push(pair_rules[&[a, b]]);
-		}
-		new_polymer.push(*int_polymer.last().unwrap());
-		swap(int_polymer, new_polymer);
-	}
-	count_polymer(&int_polymer[1..(&int_polymer.len() - 1)])
-}
-
-fn count_polymer(polymer: &[u8]) -> Vec<(u8, u128)> {
-	let counts = polymer.iter().copied().counts();
-	counts.citer().map(|(k, v)| (k, v as u128)).collect()
-}
-
-#[allow(dead_code)]
-fn display_polymer(polymer: &[u8]) {
-	display(polymer.iter().map(|&a| a as char).collect::<String>());
+	new_map
 }
